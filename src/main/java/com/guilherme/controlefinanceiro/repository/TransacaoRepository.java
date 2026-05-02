2b357bfb2efb8e03897f6d61086fbf9ac4965d92
Guilhermehriq <contatoguilhermehrp@gmail.com>
2026-05-02 16:40:28 -0300

    feat: implement transaction service

--- src/main/java/com/guilherme/controlefinanceiro/repository/TransacaoRepository.java
+++ src/main/java/com/guilherme/controlefinanceiro/repository/TransacaoRepository.java
@@ -2,7 +2,6 @@ package com.guilherme.controlefinanceiro.repository;
 
 import com.guilherme.controlefinanceiro.model.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+// Intermédio do banco de dados
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
-
 }
--- src/main/java/com/guilherme/controlefinanceiro/service/TransacaoService.java
+++ src/main/java/com/guilherme/controlefinanceiro/service/TransacaoService.java
@@ -1,8 +1,19 @@
 package com.guilherme.controlefinanceiro.service;
 
+import com.guilherme.controlefinanceiro.repository.TransacaoRepository;
 import org.springframework.stereotype.Service;
-
+import com.guilherme.controlefinanceiro.model.Transacao;
 @Service
 public class TransacaoService {
-    
+
+    private final TransacaoRepository repository;
+
+    public TransacaoService(TransacaoRepository repository) {
+        this.repository = repository;
+    }
+
+    public Transacao salvar(Transacao transacao){
+        return repository.save(transacao);
+    }
+
 }
